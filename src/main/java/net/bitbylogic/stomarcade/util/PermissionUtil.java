24fb18296d22cba4a5e023cd8d031dfdd85f7433
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-12-26 06:15:13 -0500

    Allow console sender to bypass permission checks in `PermissionUtil`

--- src/main/java/net/bitbylogic/stomarcade/util/PermissionUtil.java
+++ src/main/java/net/bitbylogic/stomarcade/util/PermissionUtil.java
@@ -2,6 +2,7 @@ package net.bitbylogic.stomarcade.util;
 
 import net.bitbylogic.stomarcade.permission.container.PermissionContainer;
 import net.minestom.server.command.CommandSender;
+import net.minestom.server.command.ConsoleSender;
 import net.minestom.server.entity.Player;
 import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +14,10 @@ public class PermissionUtil {
     public PermissionUtil() {}
 
     public static boolean has(@NotNull CommandSender sender, @NotNull String permission) {
+        if (sender instanceof ConsoleSender) {
+            return true;
+        }
+
         if (!sender.hasTag(PERMISSION_CONTAINER_TAG)) {
             return false;
         }
